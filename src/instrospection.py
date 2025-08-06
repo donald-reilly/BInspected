ab58a4e372f3c359ef885947bb853b2b8e1f81d5
Donald Reilly Jr <donald.reilly.jr@outlook.com>
2025-08-06 12:57:37 -0700

    Add: __call__ to be used as a setter for the corresponding variables Add: _pull_method_args Fix: Type hinting

--- src/instrospection.py
+++ src/instrospection.py
@@ -1,7 +1,7 @@
 from collections.abc import Callable
 
 class BInspected:
-    def __init__(self, class_to_b_inspected):
+    def __init__(self, class_to_b_inspected: type):
         """
         Initializes a inspector with the class intended to be inspected.
         
@@ -10,6 +10,11 @@ class BInspected:
         """
         
         self._class_to_b_inspected = class_to_b_inspected
+        self._method_args = {}
+    def __call__(self, method_to_pull_args):
+        self._method_args = self._pull_method_args(method_to_pull_args)
+        self._method_name = method_to_pull_args.__name__
+        self._method_doc = method_to_pull_args.__doc__
     @property
     def all_methods(self)-> dict:
         """ 
@@ -50,6 +55,36 @@ class BInspected:
         """
 
         return self._pull_properties()
+    @property
+    def method_args(self)-> dict:
+        """
+        Provides access to the private variable _method_args
+        
+        Return:
+            _methods_args: (dict) A dictionary containing all the arguements neccassary for call the method provided in __call__
+        """
+        
+        return self._method_args
+    @property
+    def method_name(self)-> str:
+        """
+        Provides access to the private variable _method_name
+        
+        Return:
+            _method_name: (str) The name of the method provided to __call__
+        """
+        
+        return self._method_name        
+    @property
+    def method_doc(self)-> str:
+        """
+        Provides access to the private variable _method_doc
+        
+        Return:
+            _method_doc: (str) The name of the method provided to __call__
+        """
+        
+        return self._method_doc        
     def _pull_all_methods(self)-> dict:
         """
         Creates a dictionary containing references to the callable methods of a class
@@ -93,7 +128,7 @@ class BInspected:
         """
 
         return self._pull_provided_type(property)
-    def _pull_provided_type(self, provided_type):
+    def _pull_provided_type(self, provided_type: Callable|property):
         """
         Creates a dictionary from the classes __dict__ method, the dicitonary contains only types that match the provided type
         
@@ -105,6 +140,41 @@ class BInspected:
             for name, attributes in self._class_to_b_inspected.__dict__.items() 
             if isinstance(attributes, provided_type)}
         return _provided_type
+    def _pull_method_args(self, method_to_map: Callable)-> dict:
+        """
+        Sort this methods arguements. Provide a dictionary of each arguement and the required types.
+        
+        Params:
+            method_to_map: (Callable) The method who's arguements need to be introspected.
+        Returns:
+            mapped_args: (dict) A dictionary that becomes kwargs for class instantiation
+        """
+        
+        mapped_args = {
+            "all_vars": method_to_map.__code__.co_varnames, 
+            "default_vals": method_to_map.__defaults__, 
+            "default_kw": method_to_map.__kwdefaults__, 
+            "annotations": method_to_map.__annotations__
+        }
+        return mapped_args
+    def _parse_method_args(self, mapped_args)-> dict:
+        """
+        Parse the args required for creating an instance of the class.
+        
+        Params:
+            mapped_args:(dict) A dictionary containing all arguements of the class.
+        Returns:
+            method_args:(dict) A dictionary containing kwargs necassary for creating an instance of the class
+        """
+
+        method_args = {}
+        for arg in mapped_args['all_vars']:
+            if arg != 'self':
+                method_args[arg] = None
+        for arg in method_args.keys():
+            if arg in mapped_args:
+                method_args[arg] = mapped_args[arg]
+        return method_args
     def bview_functions(self, function_to_inspect: Callable):
         """ 
         The purpose of this method is to provide a complete mapping of the provided method or function
@@ -113,15 +183,15 @@ class BInspected:
             function_to_inspect: (Callable) The method or function to B inspected
         """
         
-        print(f"{function_to_inspect.__code__.co_varnames} = func.__code__.co_varnames")
-        print(f"{function_to_inspect.__code__.co_argcount} = func.__code__.co_argcount")
-        print(f"{function_to_inspect.__defaults__} = init.__defaults__")
-        print(f"{function_to_inspect.__kwdefaults__} = func.__kwdefaults__")
-        print(f"{function_to_inspect.__annotations__} = func.__annotations__")
-        print(f"{function_to_inspect.__doc__} = func.__doc__")
-        print(f"{function_to_inspect.__name__} = func.__name__")
-        print(f"{function_to_inspect.__module__} = func.__module__")
-        print(f"{function_to_inspect.__qualname__} = func.__qualname__")
+        function_to_inspect.__code__.co_varnames 
+        function_to_inspect.__code__.co_argcount 
+        function_to_inspect.__defaults__ 
+        function_to_inspect.__kwdefaults__ 
+        function_to_inspect.__annotations__ 
+        function_to_inspect.__doc__ 
+        function_to_inspect.__name__ 
+        function_to_inspect.__module__ 
+        function_to_inspect.__qualname__ 
     def bview_class(self, class_to_inspect):
         """
         The purpose of this function is to seperate out the functions from the __dict__ so they can be tested.
