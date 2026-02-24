a223bc3a5cbb6cd387cb1ae024b9531157f6774f
Donald Reilly Jr <donald-reilly@users.noreply.github.com>
2026-02-24 11:29:02 -0800

    Refactor: added some variables for testing

--- src/golden_class.py
+++ src/golden_class.py
@@ -3,7 +3,7 @@ class GoldenClass:
         self.class_variable = "I am a class variable"
         self._my_property = "I am a property value"
     
-    def method_one(self, param1, param2):
+    def method_one(self, param1, param2: int = 10) -> str:
         """This is method one"""
         local_variable = "I am a local variable in method one"
         return f"Method one called with {param1} and {param2}"
@@ -16,10 +16,12 @@ class GoldenClass:
     @property
     def my_property(self)-> str:
         """This is a property"""
+        some_local_variable = "I am a local variable in the property getter"
         return self._my_property
     @my_property.setter
-    def my_property(self, value: str)-> None:
+    def my_property(self, value: str = "that", other_value: str = "this", * , x, y)-> None:
         """Setter for my_property"""
+        some_local_variable = "I am a local variable in the property setter"
         self._my_property = value
     @my_property.deleter
     def my_property(self)-> None:
