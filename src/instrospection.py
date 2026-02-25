6155dd83bc2857f71eb990368637ba7fca59e873
Donald Reilly Jr <donald-reilly@users.noreply.github.com>
2026-02-24 19:50:37 -0800

    Refactor: Inital commit to major overhaul of BInspected .

--- src/instrospection.py
+++ src/instrospection.py
@@ -1,5 +1,4 @@
-from collections.abc import Callable
-import types
+
 from classifier import Classifier
 from parser import Parser
 class BInspected:
@@ -22,6 +21,79 @@ class BInspected:
             An introspection dictionary.
         """
         
+
+        return self._build_inspection(object_to_inspect)
+    def _build_inspection(self, object_to_inspect)-> dict:
+        """
+        Complete object inspection and formation of the objects underlying stucture
+        
+        Params:
+            object_to_inspect: Object to inspect
+        Returns:
+            An introspection dictionary
+        """
+
         object_type = self.classifier(object_to_inspect)
         parsed_object = self.parser(object_to_inspect, object_type)
-        return parsed_object
+        
+        return {}
+    def inspect_module(self, module_to_inspect)-> dict:
+        """
+        Inspect a modules underlying data structure and provide an introspection dictionary.
+        
+        Params:
+            module_to_inspect: Module to inspect
+        Returns:
+            An introspection dictionary
+        """
+        return {}
+    def inspect_class(self, class_to_inspect)-> dict:
+        """
+        Inspect a class' underlying data structure and provide an introspection dictionary.
+        
+        Params:
+            class_to_inspect: Class to inspect
+        Returns:
+            An introspection dictionary
+        """
+        return {}
+    def inspect_method(self, method_to_inspect)-> dict:
+        """
+        Inspect a methods underlying data structure and provide an introspection dictionary.
+        
+        Params:
+            method_to_inspect: Method to inspect
+        Returns:
+            An introspection dictionary
+        """
+        return {}
+    def inspect_function(self, function_to_inspect)-> dict:
+        """
+        Inspect a functions underlying data structure and provide an introspection dictionary.
+        
+        Params:
+            function_to_inspect: Function to inspect
+        Returns:
+            An introspection dictionary
+        """
+        return {}
+    def inspect_varibales(self, variables_to_inspect)-> dict:
+        """
+        Inspect a varibales underlying data structure and provide an introspection dictionary.
+        
+        Params:
+            variable_to_inspect: Variable to inspect
+        Returns:
+            An introspection dictionary
+        """
+        return {}
+    def inspect_arguements(self, arguements_to_inspect)-> dict:
+        """
+        Inspect an arguements underlying data structure and provide an introspection dictionary.
+        
+        Params:
+            arguement_to_inspect: Arguement to inspect
+        Returns:
+            An introspection dictionary
+        """
+        return {}
