3a6b29fee7f31eaa47755bd1e531efc2a9457be2
Donald Reilly Jr <donald-reilly@users.noreply.github.com>
2026-03-07 11:03:24 -0800

    Rewrite:  Type hint changes, _inspect generator function.

--- src/binspected/binspected.py
+++ src/binspected/binspected.py
@@ -1,18 +1,33 @@
-from types import BuiltinFunctionType, GeneratorType, BuiltinMethodType, MethodDescriptorType, MethodWrapperType, FunctionType, MethodType, WrapperDescriptorType, CodeType, NoneType
+from typing import Generator, Any
 from figman import FigMan, MasterGroup
 
 class BInspected:
     """
     BInspected is a python object introspeciton engine.
     """
-    def __init__(self, object_to_be_inspected):
+    def __init__(self, object_to_be_inspected: Any) -> None:
         self.object_to_be_inspect = object_to_be_inspected
         self.figman = FigMan()
-    def __call__(self, call):
+
+    def __call__(self):
         pass
     def _build_inspection(self)-> MasterGroup:
         pass
-    def _inspecct(self)-> GeneratorType:
-        pass
+    def _inspect(self, object_to_inspect: Any)-> Generator[tuple[str, str], Any]:
+        """
+        Create a generator function that yeilds the namespace attributes of an object.
+        
+        Params:
+            object_to_inspect
+        Returns:
+            Generator function yeilding key value pairs of the objects namespace
+        """
+
+        for name in dir(object_to_inspect):
+            try:
+                meta_data = getattr(object_to_inspect, name)
+            except:
+                continue
+            yield name, meta_data
     def _calssifier(self)-> str:
         pass
