41f73391705d8bf9744bc5ddef9639c96a62dada
Donald Reilly Jr <donald-reilly@users.noreply.github.com>
2026-03-07 12:01:33 -0800

    Rewrite: Cleaned up __init__

--- src/binspected/binspected.py
+++ src/binspected/binspected.py
@@ -1,14 +1,24 @@
 from typing import Generator, Any
 from figman import FigMan, MasterGroup
+from binspected.bclassified import BClassified
 
 class BInspected:
     """
     BInspected is a python object introspeciton engine.
     """
     def __init__(self, object_to_be_inspected: Any) -> None:
-        self.object_to_be_inspect = object_to_be_inspected
-        self.figman = FigMan()
+        """
+        Initialize the BInspected class and it's components.
+        
+        Params:
+            object_to_be_inspected
+        Returns:
+            None
+        """
 
+        self.object_to_be_inspect = object_to_be_inspected# Assigns the object to be inspected to a instance attribute.
+        self.figman = FigMan()# Initialize the dictionary manager.
+        self.classifier = BClassified()# Initialize the classifier.
     def __call__(self):
         pass
     def _build_inspection(self)-> MasterGroup:
@@ -29,5 +39,5 @@ class BInspected:
             except:
                 continue
             yield name, meta_data
-    def _calssifier(self)-> str:
+    def _classifier(self)-> str:
         pass
