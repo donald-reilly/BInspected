ce9af8f380f49add564d92210f7b612b40974935
Donald Reilly Jr <donald-reilly@users.noreply.github.com>
2026-03-07 12:01:01 -0800

    Rewrite: Classification logic improved

--- src/binspected/bclassified.py
+++ src/binspected/bclassified.py
@@ -1,4 +1,4 @@
-from typing import Generator
+from typing import Generator, Any
 from types import BuiltinFunctionType, GeneratorType, BuiltinMethodType, MethodDescriptorType, MethodWrapperType, FunctionType, MethodType, WrapperDescriptorType, CodeType, NoneType
 
 class BClassified:
@@ -6,16 +6,27 @@ class BClassified:
     BClassified provides an single point of classification and grouping of python objects.
     """
     def __init__(self) -> None:
-        self.branch = {
-            "Method Descriptor": MethodDescriptorType,
-            "Builtin Function": BuiltinFunctionType,
-            "Builtin Method": BuiltinMethodType,
-            "Method Wrapper": MethodWrapperType,
-            "Wrapper Descriptor": WrapperDescriptorType,
-            "Function": FunctionType,
-            "Method": MethodType,
-            "Generator": Generator
+        """
+        Initialize the BClassified class and it's components.
+        
+        Params:
+            None
+        Returns:
+            None
+        """
+
+        # A collection of known types that could be recursed into.
+        self.branch: dict[Any, str] = {
+            MethodDescriptorType: "Method Descriptor",
+            BuiltinFunctionType: "Builtin Function",
+            BuiltinMethodType: "Builtin Method",
+            MethodWrapperType: "Method Wrapper",
+            WrapperDescriptorType: "Wrapper Descriptor",
+            FunctionType: "Function",
+            MethodType: "Method",
+            Generator: "Generator"
         }
+        # A collection of known nodes in the mapping.
         self.leaf = (
             int,
             float,
@@ -29,6 +40,21 @@ class BClassified:
             set,
             frozenset
         )
-    def __call__(self, object_to_classify):
-        pass
-    def _classify(self, object_to_classify):
+    def __call__(self, name: str, object_to_classiy: Any)-> tuple[str, str]:
+        return self._classify(object_to_classify)
+    def _classify(self, name: str, object_to_classify: Any)->tuple[str, str]:
+        object_type: Any = type(object_to_classify)
+        if object_type in self.branch:
+            object_classification = self.branch[object_type]
+        elif object_type in self.leaf:
+            object_classification = "Attribute"
+        else:
+            object_classification = "Unknown"
+        if name.startswith("__") and name.endswith("__"):
+            subclass = "Special"
+        elif name.startswith("_"):
+            subclass = "Private"
+        else:
+            subclass = "Public"
+        return object_classification, subclass
+
