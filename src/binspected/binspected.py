60a88b2e31978ae9754a069ca92329e51966e8cd
Donald Reilly Jr <donald-reilly@users.noreply.github.com>
2026-03-07 13:48:36 -0800

    Rewrite:  Added _build_inspected function

--- src/binspected/binspected.py
+++ src/binspected/binspected.py
@@ -16,13 +16,22 @@ class BInspected:
             None
         """
 
-        self.object_to_be_inspect = object_to_be_inspected# Assigns the object to be inspected to a instance attribute.
+        self.object_to_inspect = object_to_be_inspected# Assigns the object to be inspected to a instance attribute.
         self.figman = FigMan()# Initialize the dictionary manager.
+        self.introspection_dict = self.figman.configuration(str(self.object_to_inspect))
         self.classifier = BClassified()# Initialize the classifier.
+        self._build_inspection()
     def __call__(self):
         pass
     def _build_inspection(self)-> MasterGroup:
-        pass
+        for name, value in self._inspect(self.object_to_inspect):
+            print(f"{name}, {value}")
+            classification, subclass = self.classifier(name, value)
+            print(f"{classification}, {subclass}")
+            new_member = self.introspection_dict(classification)
+            sub_member = new_member(subclass)
+            if sub_member:
+                sub_member(name, value)
     def _inspect(self, object_to_inspect: Any)-> Generator[tuple[str, str], Any]:
         """
         Create a generator function that yeilds the namespace attributes of an object.
@@ -39,5 +48,3 @@ class BInspected:
             except:
                 continue
             yield name, meta_data
-    def _classifier(self)-> str:
-        pass
